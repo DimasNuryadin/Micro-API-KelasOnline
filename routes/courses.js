2fb394c830de29157636c6c52f7dc7309be8b543
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-02-01 15:30:45 +0700

    done create API Gateway courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { APP_NAME } = process.env;
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send("courses");
-});
+const coursesHandler = require('./handler/courses');
+const verifyToken = require('../middlewares/verifyToken')
+
+router.get('/', coursesHandler.getAll);
+router.get('/:id', coursesHandler.get);
+router.post('/', verifyToken, coursesHandler.create);
+router.put('/:id', verifyToken, coursesHandler.update);
+router.delete('/:id', verifyToken, coursesHandler.destroy);
 
 module.exports = router;
