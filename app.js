7bd1033cbfb3309aace06d3acebfc02b09e70a5d
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-02-02 14:40:38 +0700

    done create API lessons

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const logger = require('morgan');
 // Library tambahan
 require('dotenv').config();
 
+// Router
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const courseRouter = require('./routes/courses');
 const chaptersRouter = require('./routes/chapters');
+const lessonsRouter = require('./routes/lessons');
 const mediaRouter = require('./routes/media');
 const ordersRouter = require('./routes/orders');
 const paymentsRouter = require('./routes/payments');
@@ -30,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courses', courseRouter);
 app.use('/chapters', verifyToken, chaptersRouter);
+app.use('/lessons', lessonsRouter);
 app.use('/media', mediaRouter);
 app.use('/orders', ordersRouter);
 app.use('/payments', paymentsRouter);
