42f809b82f9f7e02a420fac3ec06fb00110756b6
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-02-10 19:23:56 +0700

    done create API Gateway webhook

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const mentorsRouter = require('./routes/mentors');
 const imageCoursesRouter = require('./routes/imageCourses');
 const myCoursesRouter = require('./routes/myCourses');
 const reviewsRouter = require('./routes/reviews');
+const webhookRouter = require('./routes/webhook');
 
 const verifyToken = require('./middlewares/verifyToken');
 
@@ -44,5 +45,6 @@ app.use('/mentors', verifyToken, mentorsRouter);
 app.use('/image-courses', verifyToken, imageCoursesRouter);
 app.use('/my-courses', verifyToken, myCoursesRouter);
 app.use('/reviews', verifyToken, reviewsRouter);
+app.use('/webhook', webhookRouter);
 
 module.exports = app;
